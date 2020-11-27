f5dc567b64bbe665ad6dd40f586cec18807ecfea
Павел Таньчев <p.tanchev@spider.ru>
2020-11-27 18:24:24 +0500

    DRF auth
    * config.settings.base: Token auth only by default
    * config.settings.local: Added drf settings for dev
    * skills_factory.main.views: Desabled auth

--- skills_factory/main/views.py
+++ skills_factory/main/views.py
@@ -1,8 +1,5 @@
-from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
 from django.views import generic
 
 
-@method_decorator(login_required, name="dispatch")
 class HomeView(generic.TemplateView):
     template_name = "pages/home.html"
